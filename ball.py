6bfbdf6d2c1be6a2d8a5bb952b9300755e1bec44
Akshat Girdhar <84849106+coderakg@users.noreply.github.com>
2022-12-06 17:50:56 +0530

    Update ball.py

--- ball.py
+++ ball.py
@@ -10,9 +10,9 @@ class Ball(Turtle):
         self.penup()
         self.goto(0,0)
         self.shapesize(stretch_wid=1,stretch_len=1)
-        self.x_move = random.randint(5,20) # defining a random number that will be added as we move the ball
+        self.x_move = random.randint(5,20) 
         self.y_move = random.randint(5,20) 
-        self.move_speed = 0.1 # initialising the speed of the ball
+        self.move_speed = 0.1 
         
     def move(self):
         new_x = self.xcor() + self.x_move
@@ -20,16 +20,15 @@ class Ball(Turtle):
         self.goto(new_x,new_y)
         
     def bounce_y(self):
-        self.y_move *= -1 # now the direction will be reveresed and the ball will 
+        self.y_move *= -1 
         
     def bounce_x(self):
         self.x_move *= -1
-        self.move_speed *= 0.9 #everytime it hits the paddle it will increase speed
+        self.move_speed *= 0.9 
         
     def reset_ball(self):
         self.goto(0,0)
         self.move_speed = 0.1
-        self.bounce_x()
-        
+        self.bounce_x()    
         
     
